9534a1d52828606bebd5a91394077a1f98f85cbf
Emetselch1984 <banzai.nippon2030@gmail.com>
2021-02-07 05:34:22 +0900

    Use before method

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,12 +1,13 @@
 require_relative 'message_filter'
 
 describe MessageFilter do
+  before do
+    @filter = MessageFilter.new('foo')
+  end
   it "NGワードの検査" do
-    filter = MessageFilter.new('foo')
-    expect(filter.detect?('hello from foo')).to be_truthy
+    expect(@filter.detect?('hello from foo')).to be_truthy
   end
   it "NGワードがなかったら検査しない" do
-    filter = MessageFilter.new('foo')
-    expect(filter.detect?('hello from fao')).to be_falsey
+    expect(@filter.detect?('hello from fao')).to be_falsey
   end
 end
