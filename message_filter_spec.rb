2244850fd35d981df99c7757a175f177bb7e9235
Emetselch1984 <banzai.nippon2030@gmail.com>
2021-02-07 07:24:12 +0900

    Finished practice_rspec0207

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -7,6 +7,12 @@ shared_examples "MessageFilter with argument 'foo'" do
   it "NGワードがなかったら検査しない" do
     is_expected.not_to be_detect('hello from fao')
   end
+  it "引数が空である" do
+    expect(subject.ng_words).not_to be_empty
+  end
+  it "nq_wordsは１つである" do
+    expect(filter.ng_words.size).to be 1
+  end
 end
 shared_examples "MessageFilter with argument 'foo','bar'" do
   it "複数NGワードの検査" do
@@ -15,15 +21,18 @@ shared_examples "MessageFilter with argument 'foo','bar'" do
   it "複数NGワードがなかったら検査しない" do
     is_expected.not_to be_detect('hello from fao')
   end
+  it "引数が空である" do
+    expect(subject.ng_words).not_to be_empty
+  end
+  it "nq_wordsは2つである" do
+    expect(filter.ng_words.size).to be 2
+  end
 end
 
 describe MessageFilter do
   context "with argument 'foo'" do
     let(:filter){ MessageFilter.new('foo') }
     subject {filter}
-    it "引数が空である" do
-      expect(subject.ng_words).not_to be_empty
-    end
     it_behaves_like "MessageFilter with argument 'foo'"
   end
   context "with argument 'foo bar'" do
