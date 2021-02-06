f84e92c0a348aecef7ed5c3e281b59740d11339c
Emetselch1984 <banzai.nippon2030@gmail.com>
2021-02-07 06:11:37 +0900

    fix rspec

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,14 +1,14 @@
 require_relative 'message_filter'
 
 describe MessageFilter,"with argument 'foo'" do
-  before do
-    @filter = MessageFilter.new('foo')
-  end
-  subject {@filter}
+
+  let(:filter){ MessageFilter.new('foo') }
+  subject {filter}
+
   it "NGワードの検査" do
     is_expected.to be_detect('hello from foo')
   end
   it "NGワードがなかったら検査しない" do
-    is_expected.to be_detect('hello from fao')
+    is_expected.not_to be_detect('hello from fao')
   end
 end
