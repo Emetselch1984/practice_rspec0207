deb8c6417a16102344cb84ad924ba514ce4b4cc9
Emetselch1984 <banzai.nippon2030@gmail.com>
2021-02-07 05:06:44 +0900

    Add first spec

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,5 +1,8 @@
 require_relative 'message_filter'
 
 describe MessageFilter do
-
+  it "NGワードの検査" do
+    filter = MessageFilter.new('foo')
+    expect(filter.detect?('hello from foo')).to be_truthy
+  end
 end
