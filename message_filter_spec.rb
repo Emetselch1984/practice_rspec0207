e733b74f12636fed354c175cb7d431bb4c2eb7f8
Emetselch1984 <banzai.nippon2030@gmail.com>
2021-02-07 06:48:24 +0900

    Use shared_examples

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,10 +1,6 @@
 require_relative 'message_filter'
 
-describe MessageFilter,"with argument 'foo'" do
-
-  let(:filter){ MessageFilter.new('foo') }
-  subject {filter}
-
+shared_examples "MessageFilter with argument 'foo'" do
   it "NGワードの検査" do
     is_expected.to be_detect('hello from foo')
   end
@@ -12,15 +8,24 @@ describe MessageFilter,"with argument 'foo'" do
     is_expected.not_to be_detect('hello from fao')
   end
 end
-describe MessageFilter,"with argument 'foo bar'" do
-
-  let(:filter){ MessageFilter.new('foo','bar') }
-  subject {filter}
-
+shared_examples "MessageFilter with argument 'foo','bar'" do
   it "複数NGワードの検査" do
     is_expected.to be_detect('hello from foo')
   end
   it "複数NGワードがなかったら検査しない" do
     is_expected.not_to be_detect('hello from fao')
   end
+end
+
+describe MessageFilter,"with argument 'foo'" do
+
+  let(:filter){ MessageFilter.new('foo') }
+  subject {filter}
+  it_behaves_like "MessageFilter with argument 'foo'"
+end
+describe MessageFilter,"with argument 'foo bar'" do
+
+  let(:filter){ MessageFilter.new('foo','bar') }
+  subject {filter}
+  it_behaves_like "MessageFilter with argument 'foo','bar'"
 end
